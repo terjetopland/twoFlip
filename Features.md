0717b08f2782ee98b28e927379b339aace1423e9
Terje Topland <74875770+terjetopland@users.noreply.github.com>
2022-06-28 13:48:57 +0200

    working to construct cards

---
+++ Features.md
@@ -0,0 +1,2 @@
+[] metode som kjører når man klikker på et kort
+[] randomisere listen av kort
---
+++ Models/Card.cs
@@ -0,0 +1,7 @@
+public class Card
+{
+    public string CardValue { get; set; } = "";
+
+    public bool ValueIsVisible { get; set; }
+
+}
